f7d84aa7af36c75cb15c5fe3aa899b30527b1f3a
bhandari63 <71725367+bhandari63@users.noreply.github.com>
2022-10-11 17:55:04 -0500

    Create control-vs-ozone-network_script.R

---
+++ Network analysis/control-vs-ozone-network_script.R
@@ -0,0 +1,102 @@
+.#Network analysis is an RAM intensive job so we submitted the codes in HPC.
+
+#/bin/bash
+source /opt/asn/etc/asn-bash-profiles-special/modules.sh
+module load R/4.1.0
+R CMD BATCH inference.R
+
+#The inference.R file contains the exact script for network analysis which is as follows.
+
+library(devtools)
+library(SpiecEasi)
+library(microbiome)
+library(phyloseq)
+library(NetCoMi)
+# Load  data
+net.c1 <- readRDS("/scratch/aubrrb/netcomi/allsamples.rds")
+net.c<- subset_samples(net.c1, environment !="GH" & innoculation !="Innoculated")
+
+X10Rinnocamb_prevalent <- core(net.c, detection = 1^2, prevalence = 20/100)
+
+net.c <- prune_taxa(taxa_sums(X10Rinnocamb_prevalent) >100, X10Rinnocamb_prevalent)
+# Extract count matrix and phenotypes
+env_counts <- t(net.c@otu_table@.Data)
+colnames(env_counts) <- net.c@tax_table@.Data[, "Genus"]
+dim(env_counts)
+
+
+
+env_pheno <- net.c@sam_data@.Data
+names(env_pheno) <- net.c@sam_data@names
+
+environment <- env_pheno$environment
+
+#construct net with higherst freq more than 80
+net_env <- netConstruct(env_counts, group = environment, filtTax = "highestFreq",
+                           filtTaxPar = list(highestFreq = 80),
+                           zeroMethod = "none", normMethod = "none",
+                           measure = "spring",
+                           measurePar = list(nlambda = 50, rep.num = 50,
+                                             ncores=30),
+                           seed = 20190101)
+
+netprops_env <- netAnalyze(net_env, clustMethod = "cluster_fast_greedy",
+                              hubPar = "eigenvector", hubQuant = 0.95,
+                              lnormFit = FALSE)
+
+summary(netprops_env, groupNames = c("Ambient", "Ozone"))
+
+
+
+pdf("/scratch/aubrrb/netcomi/abioticstressonly.pdf", width = 50, height = 25)
+p <- plot(netprops_env,
+          sameLayout = TRUE,
+          layoutGroup = 1,
+          repulsion = 1,
+          groupNames = c("Ambient","Ozone"),
+          shortenLabels = "intelligent",
+          labelLength = 14,
+          labelPattern = c(14,"'",3),
+          charToRm = "g__",
+          labelScale = FALSE,
+          nodeFilter = "none",
+          nodeFilterPar = 20,
+          rmSingles = "inboth",
+          nodeSize = "eigen",
+          nodeSizeSpread = 3,
+          nodeColor = "cluster",
+          colorVec = rainbow(12),
+          nodeTransp = 65,
+          hubTransp = 50,
+          hubBorderWidth = 2,
+          hubBorderCol = "gray40",
+          colorNegAsso = TRUE,
+          edgeWidth = 1,
+          edgeTranspLow = 70,
+          edgeTranspHigh = 30,
+          cexNodes = 1,
+          cexHubs = 1.3,
+          cexTitle = 2.0,
+          cexLabels = 0.8,
+          showTitle = TRUE,
+          mar = c(1,1,3,1))
+dev.off()
+
+# Run on server with 15 CPU cores:
+net_env_comp<- netCompare(netprops_env, permTest = TRUE,
+                             lnormFit = FALSE, jaccQuant = 0.75,
+                             nPerm = 1000, cores = 30,
+                             seed = 20190101, adjust = "none")
+
+# Rerun with multiple testing adjustment
+net_env_comp_adaptbh <- netCompare(netprops_env, permTest = TRUE,
+                                      lnormFit = FALSE, jaccQuant = 0.75,
+                                      nPerm = 1000, cores = 30,
+                                      seed = 20190101, adjust = "adaptBH",
+                                      assoPerm = net_env_comp$assoPerm)
+
+summary(net_env_comp, pAdjust = TRUE,
+        groupNames = c("Ambient", "Ozone"), digitsPval = 6)
+
+summary(net_env_comp_adaptbh, pAdjust = TRUE,
+        groupNames = c("Ambient", "Ozone"), digitsPval = 6)
